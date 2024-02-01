e48d8c4af86bca419f200a766d0258b7a6e3f89f
Alvarezpro87 <alvareznuvem@gmail.com>
2024-02-01 18:15:33 -0300

    Alterado o arquivo NodeApp para aguardar a configuração do banco de dados

--- NodeApp/app.js
+++ NodeApp/app.js
@@ -1,18 +1,39 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
-
 require('dotenv').config();
 
-// Conexão com o MySQL
-const connection = mysql.createConnection({
+const dbConfig = {
   host: 'db',
   user: 'root',
   password: process.env.MYSQL_ROOT_PASSWORD,
   database: process.env.MYSQL_DATABASE
-});
+};
 
-// Rota para buscar capitais
+let connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(dbConfig); 
+
+  connection.connect(function(err) {             
+    if(err) {                                     
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); 
+    }                                     
+  });                                     
+                                          
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect();                        
+    } else {                                     
+      throw err;                                  
+    }
+  });
+}
+
+handleDisconnect();
+
 app.get('/capitais', (req, res) => {
   connection.query('SELECT * FROM Nome', (err, results) => {
     if (err) throw err;
@@ -24,3 +45,5 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor Node.js rodando na porta ${PORT}`);
 });
+
+
